16349ebf0b1743681290ba3edc7a8234360e0943
temp27-alt <tempforbnmit@gmail.com>
2026-01-30 09:47:16 +0530

    Binary Search

---
+++ main.py
@@ -0,0 +1,17 @@
+def binary_search(arr, n):
+    start = 0
+    end = len(arr) - 1
+    while (start <= end):
+        mid = (start + end) // 2
+        if (n == arr[mid]):
+            print(f"{n} is Found at index: {mid}")
+            return
+        if (n > arr[mid]):
+            start = mid + 1
+        if (n < arr[mid]):
+            end = mid - 1
+    print(f"{n} is Not Found")
+
+
+arr = [51, 100, 231, 400]
+binary_search(arr, 231)
