b8787b9bb3453c85c2146b6faf4b7a3d0e4b1210
Abhiram H <abhiramh21@gmail.com>
2026-04-21 21:09:09 +0530

    update

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 def binary_search(arr, n):
     start = 0
     end = len(arr) - 1
-    while (start <= end):
+    while start <= end:
         mid = (start + end) // 2
-        if (n == arr[mid]):
+        if n == arr[mid]:
             print(f"{n} is Found at index: {mid}")
             return
-        if (n > arr[mid]):
+        if n > arr[mid]:
             start = mid + 1
-        if (n < arr[mid]):
+        if n < arr[mid]:
             end = mid - 1
     print(f"{n} is Not Found")
 
 
-arr = [70, 100, 231, 400]
+arr = [50, 100, 231, 400]
 binary_search(arr, 400)
